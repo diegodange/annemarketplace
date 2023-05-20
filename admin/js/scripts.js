0f3ba273bf0363c35f19a5966d1e76fee73c504f
Diego Antoniança <diegoantonianca@hotmail.com>
2023-05-20 16:55:16 -0300

    Remoção Opções Pessoais

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -3,6 +3,19 @@ jQuery(document).ready(function($) {
     if (activityPanel.length > 0) {
         activityPanel.remove();
     }
+
+    $('#your-profile h2').each(function() {
+        if ($(this).text() === 'Opções pessoais') {
+          $(this).remove();
+        }
+    });
+    
+    $('.user-rich-editing-wrap').remove();
+    $('.user-comment-shortcuts-wrap').remove();
+    $('.user-admin-bar-front-wrap').remove();
+    $('.user-language-wrap').remove();
+    $('#application-passwords-section').remove();
+      
     $('#_vendor_id').select2({
         maximumSelectionLength: 1
     } );
