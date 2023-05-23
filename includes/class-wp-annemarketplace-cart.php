8bdc9c6fc1b50f3e40b427c4e0599ca10749c186
Diego Antoniança <diego.antonianca.ext@espm.br>
2023-05-23 18:44:55 -0300

    Estrutura para compra ùnica

--- includes/class-wp-annemarketplace-cart.php
+++ includes/class-wp-annemarketplace-cart.php
@@ -11,12 +11,11 @@ class Cart{
         return self::$instance;
     }
 
-
     public function __construct() {
-        add_action('woocommerce_check_cart_items', [$this,'verificar_vendedores_no_carrinho']);
+        add_action('woocommerce_proceed_to_checkout', [$this,'verificar_vendedores_no_carrinho']);
+        add_action('woocommerce_before_cart', [$this,'exibir_mensagem_erro_vendedores']);
     }
 
-    // Verificar se o carrinho contém produtos de múltiplos vendedores
     function verificar_vendedores_no_carrinho() {
         $cart_items = WC()->cart->get_cart();
 
@@ -30,7 +29,27 @@ class Cart{
                 if ($first_vendor_id === null) {
                     $first_vendor_id = $vendor_id;
                 } elseif ($vendor_id !== $first_vendor_id) {
-                    wc_add_notice(__('Você só pode comprar produtos de um único vendedor por vez.'), 'error');
+                    remove_action('woocommerce_proceed_to_checkout', 'woocommerce_button_proceed_to_checkout', 20);
+                    break;
+                }
+            }
+        }
+    }   
+
+    function exibir_mensagem_erro_vendedores() {
+        $cart_items = WC()->cart->get_cart();
+
+        if (count($cart_items) > 1) {
+            $first_vendor_id = null;
+
+            foreach ($cart_items as $cart_item_key => $cart_item) {
+                $product_id = $cart_item['product_id'];
+                $vendor_id = get_post_meta($product_id, '_vendor_id', true);
+
+                if ($first_vendor_id === null) {
+                    $first_vendor_id = $vendor_id;
+                } elseif ($vendor_id !== $first_vendor_id) {
+                    wc_print_notice(__('Você só pode comprar produtos de um único vendedor por vez.'), 'error');
                     break;
                 }
             }
