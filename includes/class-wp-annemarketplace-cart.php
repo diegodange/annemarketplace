18bdd125dce0bdbe3361a2c281e490f3c97e356f
Diego Antoniança <diego.antonianca.ext@espm.br>
2023-07-24 22:51:26 -0300

    Nome do Produtor no Carrinho AJUSTE

--- includes/class-wp-annemarketplace-cart.php
+++ includes/class-wp-annemarketplace-cart.php
@@ -12,26 +12,28 @@ class Cart{
     }
 
     public function __construct() {
-        add_filter('woocommerce_cart_item_name', [$this,'adicionar_informacoes_vendedor_carrinho'], 10, 3);
         add_action('woocommerce_single_product_summary', [$this,'adicionar_informacoes_vendedor_pagina_produto'], 6);
+        add_filter('woocommerce_get_item_data', [$this,'preencher_coluna_nome_loja_carrinho'], 10, 2);
     }
 
-    // Adiciona as informações do vendedor aos produtos do carrinho
-    function adicionar_informacoes_vendedor_carrinho($product_name, $cart_item, $cart_item_key) {
+    // Preencher a coluna com o nome da loja ou nome do dono do produto
+    function preencher_coluna_nome_loja_carrinho($cart_item_data, $cart_item) {
         $product_id = $cart_item['product_id'];
         $vendor_id = get_post_field('post_author', $product_id);
         $shop_name = get_user_meta($vendor_id, 'store_name', true);
-        $vendor_name = get_the_author_meta('display_name', $vendor_id);
+
+        if (empty($shop_name)) {
+            $shop_name = get_the_author_meta('display_name', $vendor_id);
+        }
 
         if ($shop_name) {
-            $product_name .= '<br><small><b>Produtor </b> ' . $shop_name . '</small>';
+            $cart_item_data['nome_loja'] = array(
+                'key'   => 'Produtor',
+                'value' => $shop_name,
+            );
         }
 
-        if (!$shop_name) {
-            $product_name .= '<br><small><b>Produtor </b> ' . $vendor_name . '</small>';
-        }
-
-        return $product_name;
+        return $cart_item_data;
     }
 
     // Adiciona as informações do vendedor na página do produto
@@ -46,7 +48,7 @@ class Cart{
             echo '<div class="vendor-info"><b>Produtor:</b> ' . $shop_name . '</div>';
         }
 
-        if (!$shop_name) {
+        if (empty($shop_name)) {
             echo '<div class="vendor-info"><b>Vendedor:</b> ' . $vendor_name . '</div>';
         }
     }
